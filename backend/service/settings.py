4210534babf653ca7379f16e04079cefd9afe42b
SKairinos <skairinos@gmail.com>
2023-09-15 17:17:24 +0100

    remove extra white spacing

--- backend/service/settings.py
+++ backend/service/settings.py
@@ -77,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "service.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -87,7 +86,6 @@ DATABASES = {
         "NAME": Path(os.getenv("DB_NAME", BASE_DIR / "db.sqlite3")).resolve(),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -107,20 +105,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
