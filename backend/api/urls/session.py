681750f90e6670a497d99609caf2e9c570fa7357
SKairinos <skairinos@gmail.com>
2023-09-15 11:37:36 +0100

    remove logout endpoint

--- backend/api/urls/session.py
+++ backend/api/urls/session.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from ..views.session import ClearExpiredView, LoginView
@@ -8,11 +7,6 @@ urlpatterns = [
         "login/",
         LoginView.as_view(),
         name="login",
-    ),
-    path(
-        "logout/",
-        LogoutView.as_view(),
-        name="logout",
     ),
     path(
         "clear-expired/",
