e68808597c8a2ea15f7ef777fa6c0b0a3e6e9f18
SKairinos <skairinos@gmail.com>
2023-09-15 17:15:53 +0100

    remove  login middleware

--- backend/service/settings.py
+++ backend/service/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "api.middlewares.LoginMiddleware"
 ]
 
 ROOT_URLCONF = "service.urls"
