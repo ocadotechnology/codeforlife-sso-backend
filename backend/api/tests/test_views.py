db83876c7c118f9d8663305479b274f367e0c4d1
SKairinos <skairinos@gmail.com>
2023-09-29 16:31:50 +0100

    test otp

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -1,7 +1,50 @@
 from unittest.mock import Mock, patch
 
+import pyotp
 from codeforlife.tests import CronTestCase
+from codeforlife.user.models import AuthFactor, User
+from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
+
+
+class TestLoginView(TestCase):
+    def setUp(self):
+        self.user = User.objects.get(id=2)
+
+    def test_post__otp(self):
+        AuthFactor.objects.create(
+            user=self.user,
+            type=AuthFactor.Type.OTP,
+        )
+
+        response = self.client.post(
+            reverse("login", kwargs={"form": "email"}),
+            data={
+                "email": self.user.email,
+                "password": "Password1",
+            },
+        )
+
+        assert response.status_code == 200
+        self.assertDictEqual(
+            response.json(), {"auth_factors": [AuthFactor.Type.OTP]}
+        )
+
+        self.user.userprofile.otp_secret = pyotp.random_base32()
+        self.user.userprofile.save()
+
+        totp = pyotp.TOTP(self.user.userprofile.otp_secret)
+
+        now = timezone.now()
+        with patch.object(timezone, "now", return_value=now):
+            response = self.client.post(
+                reverse("login", kwargs={"form": "otp"}),
+                data={"otp": totp.at(now)},
+            )
+
+        assert response.status_code == 200
+        self.assertDictEqual(response.json(), {"auth_factors": []})
 
 
 class TestClearExpiredView(CronTestCase):
