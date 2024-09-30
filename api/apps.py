97e4de0a14e2287c45ffda62398d01675e1011c9
SKairinos <skairinos@gmail.com>
2024-09-30 07:52:12 +0000

    remove signals

--- api/apps.py
+++ api/apps.py
@@ -9,7 +9,3 @@ from django.apps import AppConfig
 # pylint: disable-next=missing-class-docstring
 class ApiConfig(AppConfig):
     name = "api"
-
-    def ready(self):
-        # pylint: disable-next=import-outside-toplevel,unused-import
-        from . import signals
