aaa2acb974c9e90bf6a5c9dece3a361ef7e5ffb2
Stefan Kairinos <skairinos@gmail.com>
2024-09-30 11:10:13 +0100

    Sso 19 (#20)
    * delete frontend folder
    
    * rename backend folder to api
    
    * fix root-level files
    
    * add .gitkeep
    
    * fix git ignore
    
    * fix api
    
    * add
    
    * new cfl package
    
    * new cfl package
    
    * use backend workflow
    
    * remove signals

---
+++ api/apps.py
@@ -0,0 +1,11 @@
+"""
+© Ocado Group
+Created on 02/07/2024 at 11:58:24(+01:00).
+"""
+
+from django.apps import AppConfig
+
+
+# pylint: disable-next=missing-class-docstring
+class ApiConfig(AppConfig):
+    name = "api"
