01e012276a1aab2d4ccdebd7bfda67db7e531477
SKairinos <skairinos@gmail.com>
2025-08-01 10:59:31 +0000

    fix

--- src/tasks/session_test.py
+++ src/tasks/session_test.py
@@ -12,8 +12,8 @@ from django.core.management import call_command as _call_command
 
 
 class TestSession(CeleryTestCase):
-    @patch("src.sso.tasks.session.call_command", side_effect=_call_command)
+    @patch("src.tasks.session.call_command", side_effect=_call_command)
     def test_clear_sessions(self, call_command: Mock):
         """Can clear all expired sessions."""
-        self.apply_task("src.sso.tasks.session.clear")
+        self.apply_task("src.tasks.session.clear")
         call_command.assert_called_once_with("clearsessions")
