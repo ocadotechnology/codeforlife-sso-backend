b9cfb1ced6622eec00acd7678229a6b52d0cdd0d
SKairinos <skairinos@gmail.com>
2025-08-15 14:23:55 +0000

    fix

--- settings.py
+++ settings.py
@@ -24,3 +24,5 @@ secrets = set_up_settings(BASE_DIR, service_name="sso")
 from codeforlife.settings import *
 
 SECRET_KEY = secrets.SECRET_KEY
+# TODO: remove this when cfl-common is not longer installed
+ENCRYPTION_KEY = SECRET_KEY
